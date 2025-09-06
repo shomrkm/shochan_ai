26b3489468878a6ef6e847c6807b75e89a4181ce
shomrkm <shochan1985@gmail.com>
2025-09-06 09:34:30 +0900

    refactor: Standardize formatting and improve readability in Thread class methods

--- src/thread/thread.ts
+++ src/thread/thread.ts
@@ -1,41 +1,45 @@
-
 export interface Event {
-    type: string;
-    data: any;
+  type: string;
+  data: any;
 }
 
 export class Thread {
-    events: Event[] = [];
+  events: Event[] = [];
 
-    constructor(events: Event[]) {
-        this.events = events;
-    }
+  constructor(events: Event[]) {
+    this.events = events;
+  }
 
-    serializeForLLM() {
-        return this.events.map(e => this.serializeOneEvent(e)).join("\n");
-    }
+  serializeForLLM() {
+    return this.events.map((e) => this.serializeOneEvent(e)).join('\n');
+  }
 
-    trimLeadingWhitespace(s: string) {
-        return s.replace(/^[ \t]+/gm, '');
-    }
+  trimLeadingWhitespace(s: string) {
+    return s.replace(/^[ \t]+/gm, '');
+  }
 
-    serializeOneEvent(e: Event) {
-        return this.trimLeadingWhitespace(`
+  serializeOneEvent(e: Event) {
+    return this.trimLeadingWhitespace(`
             <${e.data?.intent || e.type}>
             ${
-            typeof e.data !== 'object' ? e.data :
-            Object.keys(e.data).filter(k => k !== 'intent').map(k => `${k}: ${e.data[k]}`).join("\n")}
+              typeof e.data !== 'object'
+                ? e.data
+                : Object.keys(e.data)
+                    .filter((k) => k !== 'intent')
+                    .map((k) => `${k}: ${e.data[k]}`)
+                    .join('\n')
+            }
             </${e.data?.intent || e.type}>
-        `)
-    }
+        `);
+  }
 
-    awaitingHumanResponse(): boolean {
-        const lastEvent = this.events[this.events.length - 1];
-        return ['request_more_information', 'done_for_now'].includes(lastEvent.data.intent);
-    }
+  awaitingHumanResponse(): boolean {
+    const lastEvent = this.events[this.events.length - 1];
+    return ['request_more_information', 'done_for_now'].includes(lastEvent.data.intent);
+  }
 
-    awaitingHumanApproval(): boolean {
-        const lastEvent = this.events[this.events.length - 1];
-        return lastEvent.data.intent === 'create_task';
-    }
+  awaitingHumanApproval(): boolean {
+    const lastEvent = this.events[this.events.length - 1];
+    return lastEvent.data.intent === 'create_task';
+  }
 }
